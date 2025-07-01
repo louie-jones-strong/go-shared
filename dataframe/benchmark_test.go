3eb0a394b10a230c23e6be0e0c77878a1ef0781c
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2025-07-01 20:44:46 +0100

    benchmarking

---
+++ dataframe/benchmark_test.go
@@ -0,0 +1,33 @@
+package dataframe
+
+import (
+	"testing"
+
+	"github.com/louie-jones-strong/go-shared/dataframe/apptype"
+	"github.com/louie-jones-strong/go-shared/dataframe/series"
+	"github.com/stretchr/testify/assert"
+)
+
+func Benchmark(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		df := New(
+			[]*series.Series{
+				series.New("A", apptype.String, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"}),
+				series.New("B", apptype.Int, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}),
+				series.New("C", apptype.Float, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}),
+				series.New("D", apptype.Bool, []bool{true, false, true, false, true, false, true, false, true, false, true}),
+				series.New("D_2", apptype.Bool, []bool{true, false, true, false, true, false, true, false, true, false, true}),
+			},
+		)
+		assert.NotNil(b, df)
+
+		err := df.DropColumn("D_2")
+		assert.NoError(b, err)
+
+		describeDf := df.Describe()
+		assert.NotNil(b, describeDf)
+
+		describeDf.Print(true, true, true, "Describe")
+	}
+}
