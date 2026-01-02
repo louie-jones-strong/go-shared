f227d6c4dcbfd00b6379c1574648872b8b92f67f
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2026-01-02 13:23:41 +0000

    support non list StorageEventCaller

--- storage/storage-event-caller.go
+++ storage/storage-event-caller.go
@@ -6,38 +6,35 @@ type EventCallableStorageItem interface {
 }
 
 type StorageEventCaller[M EventCallableStorageItem] struct {
-	subStore Storage[[]M]
+	subStore Storage[M]
 }
 
-func NewStorageEventCaller[M EventCallableStorageItem](subStore Storage[[]M]) *StorageEventCaller[M] {
+func NewStorageEventCaller[M EventCallableStorageItem](subStore Storage[M]) *StorageEventCaller[M] {
 	return &StorageEventCaller[M]{
 		subStore: subStore,
 	}
 }
 
-func (s *StorageEventCaller[M]) Save(items []M) error {
-	for _, item := range items {
-		err := item.OnSave()
-		if err != nil {
-			return err
-		}
+func (s *StorageEventCaller[M]) Save(obj M) error {
+	err := obj.OnSave()
+	if err != nil {
+		return err
 	}
-	return s.subStore.Save(items)
+	return s.subStore.Save(obj)
 }
 
-func (s *StorageEventCaller[M]) Load() ([]M, error) {
+func (s *StorageEventCaller[M]) Load() (M, error) {
+	var output M
 
-	items, err := s.subStore.Load()
+	obj, err := s.subStore.Load()
 	if err != nil {
-		return nil, err
+		return output, err
 	}
 
-	for _, item := range items {
-		err := item.OnLoad()
-		if err != nil {
-			return nil, err
-		}
+	err = obj.OnLoad()
+	if err != nil {
+		return output, err
 	}
 
-	return items, nil
+	return obj, nil
 }
